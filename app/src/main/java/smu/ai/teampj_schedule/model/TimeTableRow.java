67c341ca5d1d6645d417d9aa955fe6db27e9fbc7
happine2s <happine2s@sookmyung.ac.kr>
2025-11-29 23:20:39 +0900

    Feat: 타임테이블 관련 모델 추가

---
+++ app/src/main/java/smu/ai/teampj_schedule/model/TimeTableRow.java
@@ -0,0 +1,22 @@
+package smu.ai.teampj_schedule.model;
+
+
+public class TimeTableRow {
+    public String LINE_NUM;
+    public String FR_CODE;
+    public String STATION_CD;
+    public String STATION_NM;
+    public String TRAIN_NO;
+    public String ARRIVETIME;
+    public String LEFTTIME;
+    public String ORIGINSTATION;
+    public String DESTSTATION;
+    public String SUBWAYSNAME;
+    public String SUBWAYENAME;
+    public String WEEK_TAG;
+    public String INOUT_TAG;
+    public String FL_FLAG;
+    public String DESTSTATION2;
+    public String EXPRESS_YN;
+    public String BRANCH_LINE;
+}
